13f3a8dafe995abce375cbbd3877e36aa261edcd
M Rayhan <m.rayhan.2444@gmail.com>
2026-04-01 09:53:57 +0600

    Update config.js

--- designs/style1/config.js
+++ designs/style1/config.js
@@ -11,7 +11,7 @@ window.DESIGN = {
   name:      'North Western University',
   shortName: 'NWU Cover',
   formTitle: 'NWU ',
-  pdfFilename: 'CovGen (https://কভার.পাতা.বাংলা).pdf',
+  pdfFilename: 'Cover-1 ᴮʸ 𝓜 𝓡𝓪𝔂𝓱𝓪𝓷 (কভার.পাতা.বাংলা).pdf',
 
   /* ── Fields for share link ────────────── */
   shareFields: [
